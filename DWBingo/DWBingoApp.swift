8e606aeba098f4c374293c373d0faff2cabd046c
Japhet Tegtmeyer <japhteg@gmail.com>
2023-11-12 20:51:14 -0500

    Initial Commit

---
+++ DWBingo/DWBingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DWBingoApp.swift
+//  DWBingo
+//
+//  Created by Japhet Tegtmeyer on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DWBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
